57eb338b7dbb626eb3fbb0de78572bfe6b7a2253
Choxmi <gwcsathsara@gmail.com>
2021-07-26 19:12:26 +0530

    With Receipt Header

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const port = 8900;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+let receiptNo = 'S0102001'
+
+let receiptHeader = `
+**********************************************
+             INDIRA PHONE CENTER\n
+              T.P : 071-5568088
+            Behind the bus stand
+            New Shopping Complex
+                Deraniyagala
+            Receipt No:  `+receiptNo+`
+**********************************************\n
+`;
+
 app.post('/', (req, res) => {
     let printer = require("printer");
-
-    printer.printDirect({data: req.body.data
+    console.log(req.body);
+    printer.printDirect({data: receiptHeader + req.body.data + '\n\n\n'
         , type: 'RAW'
         , success:function(jobID){
             console.log("sent to printer with ID: "+jobID);
